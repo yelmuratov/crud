762e8853f4501ed92e64a1bc2a6a24ab419e6747
yelmuratov <yelimuratovsalimbay@gmail.com>
2024-10-29 23:03:26 -0400

    Default structure

--- crud/database/seeders/DatabaseSeeder.php
+++ crud/database/seeders/DatabaseSeeder.php
@@ -2,12 +2,13 @@
 
 namespace Database\Seeders;
 
+use App\Models\Faculty;
+use App\Models\Group;
+use App\Models\Student;
 use App\Models\User;
-use Faker\Factory as FakerFactory;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Major;
 use Illuminate\Database\Seeder;
-
-use function Laravel\Prompts\password;
+use App\Models\University;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,6 +17,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory()->count(10)->create();
+        User::factory()->count(20)->create();
+        University::factory()->count(20)->create();
+        Faculty::factory()->count(20)->create();
+        Major::factory()->count(20)->create();
+        Group::factory()->count(20)->create();
+        Student::factory()->count(20)->create();
     }
 }
---
+++ crud/database/seeders/MajorSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Major;
+
+class MajorSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Major::factory()->count(20)->create();
+    }
+}
